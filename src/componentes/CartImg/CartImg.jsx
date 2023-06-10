57675821cebb43cfc480ae527d3e65730b4a8d9d
mtoranzo <marianoeto@hotmail.com>
2023-06-10 13:38:18 -0300

    nuevo carrito

--- src/componentes/CartImg/CartImg.jsx
+++ src/componentes/CartImg/CartImg.jsx
@@ -1,9 +1,9 @@
 import './CartImg.css'
 
 const CartImg = () => {
-    const imgCarrito = "public/assets/carrito.jpg";
+    const imgCarrito = "/public/assets/carrito.jpg";
     return (
-        <div className='numcarrito'>
+        <div className='numCarrito'>
             <img className='imgCarrito' src={imgCarrito} alt="carrito de compras" />
             <strong> 10 </strong>
         </div>
