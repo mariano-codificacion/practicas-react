d9f5c83b15b3798f3d49e76bd25a1928122b1e7f
mtoranzo <marianoeto@hotmail.com>
2023-06-11 00:17:17 -0300

    cambios

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -8,18 +8,20 @@ import './NavBar.css'
 const NavBar = () => {
   return (
     <header>
-        <Navbar>
-          <Container>
-            <Navbar.Brand href="#home"><h1 className='uni text-center'>Uniformes Escolares Online</h1></Navbar.Brand>
-            <Nav className="me-auto ">
+      <Navbar>
+        <Container>
+          <div className='row'>
+            <Nav className='col-xxl-12 col-xl-12 col-md-6 col-xs-2 col-1 text-center'>
+              <Navbar.Brand href="#home"><h1 className='uni'>Uniformes Escolares</h1></Navbar.Brand>
               <Nav.Link href="#home">Uniforme Maestra</Nav.Link>
               <Nav.Link href="#features">Uniforme Maestro</Nav.Link>
               <Nav.Link href="#pricing">Uniforme Niña</Nav.Link>
-              <Nav.Link href="#pricing">Uniforme Niña</Nav.Link>
+              <Nav.Link href="#pricing">Uniforme Niño</Nav.Link>
               <CartImg />
             </Nav>
-          </Container>
-        </Navbar>
+          </div>
+        </Container>
+      </Navbar>
     </header>
   )
 }
