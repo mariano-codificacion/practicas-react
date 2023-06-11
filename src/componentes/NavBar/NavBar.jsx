9fd205685233916c36e87960f43484ba196fd92e
mtoranzo <marianoeto@hotmail.com>
2023-06-11 12:54:01 -0300

    cambio titulo

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home"><h1 className='uni'> Uniformes Escolares </h1></Navbar.Brand>
+        <Navbar.Brand href="#home"><h1 className='uni tex-center'> Uniformes Escolares </h1></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
