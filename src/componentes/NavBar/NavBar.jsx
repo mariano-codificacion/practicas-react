977246d0eed3a6ea74ac76d8e04b0fea312a0f4b
mtoranzo <marianoeto@hotmail.com>
2023-06-11 13:26:03 -0300

    color navbar

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -8,8 +8,8 @@ import './NavBar.css'
 const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home"><h1 className='uni tex-center'> Uniformes Escolares </h1></Navbar.Brand>
+      <Container className='color'>
+        <Navbar.Brand href="#home"><h1 className='uni'> Uniformes Escolares </h1></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
