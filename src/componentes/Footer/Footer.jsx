72c80926b69cc85ddd705d0cc5d3ecc9e691a76c
mtoranzo <marianoeto@hotmail.com>
2023-07-14 19:48:09 -0300

    link footer

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav';
+import {NavLink } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <>
             <Nav className="mt-4 color justify-content-center">
-                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link as={NavLink} to="/#HOME">Home</Nav.Link>
             </Nav>
             <p className="text-center mt-4 mb-4">© 2023 Mariano Toranzo, Inc. Reservados todos los derechos.</p>
         </>
